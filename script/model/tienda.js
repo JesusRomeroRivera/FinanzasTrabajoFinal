2070d22f7f7af4a9e40ed29b1de386cf9df5112c
Jesús Daniel Romero Rivera <jesusromero9609@gmail.com>
2020-11-23 13:00:47 -0500

    Agregando Header 2

---
+++ script/model/tienda.js
@@ -0,0 +1,12 @@
+class Tienda{
+    constructor(id){
+        this.id = id;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.correo = correo;
+        this.password = password;
+    }
+
+
+    
+}
---
+++ script/service/tienda.service.js
@@ -0,0 +1,16 @@
+import "../model/tienda.js";
+import { HttpClient } from '@angular/common/http';
+
+const url = "http://localhost:8080/Tiendas";
+
+class tiendaService{
+    /*constructor(){
+
+    }
+    listar(){
+        return this.http.get<Tienda>(this.url);
+    }
+    registrar(comida: Tienda) {
+        return this.http.post(this.url, comida);
+    }*/
+}
